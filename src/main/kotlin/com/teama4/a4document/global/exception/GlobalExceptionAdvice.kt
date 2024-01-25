d2c8f5faefc75c509697679622183f78fa4c6713
Myolang <myohanmyolang@gmail.com>
2024-01-25 21:16:53 +0900

    ADD Error Code, Error Object

--- src/main/kotlin/com/teama4/a4document/global/exception/GlobalExceptionAdvice.kt
+++ src/main/kotlin/com/teama4/a4document/global/exception/GlobalExceptionAdvice.kt
@@ -1,6 +1,7 @@
 package com.teama4.a4document.global.exception
 
 import com.teama4.a4document.domain.exception.ForbiddenException
+import com.teama4.a4document.system.errorobject.ErrorObject
 import io.jsonwebtoken.MalformedJwtException
 import io.jsonwebtoken.security.SignatureException
 import org.springframework.http.HttpStatus
@@ -10,11 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionAdvice {
-
-	@ExceptionHandler
-	fun test(forbiddenException: ForbiddenException): ResponseEntity<String> {
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body("hi")
-	}
 
 	@ExceptionHandler(SignatureException::class)
 	fun test2(signatureException: SignatureException): ResponseEntity<String> {
