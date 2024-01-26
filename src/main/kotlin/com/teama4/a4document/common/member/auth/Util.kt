dbe26f72a4ea336ba3d206f235e8bef2205721af
re-eun <pajeni@naver.com>
2024-01-26 17:57:14 +0900

    feat : add checkAuthor
    권한 검사, 작성자 검사

--- src/main/kotlin/com/teama4/a4document/common/member/auth/Util.kt
+++ src/main/kotlin/com/teama4/a4document/common/member/auth/Util.kt
@@ -2,8 +2,14 @@ package com.teama4.a4document.common.member.auth
 
 import com.teama4.a4document.common.member.entity.MemberEntity
 import com.teama4.a4document.common.member.exception.AuthorMismatchException
+import com.teama4.a4document.infra.security.UserPrincipal
 import com.teama4.a4document.system.errorobject.ErrorCode
+import org.springframework.security.core.GrantedAuthority
 
-fun checkAuthor(userEmail: String, member: MemberEntity) {
-    if( userEmail != member.email ) throw AuthorMismatchException(ErrorCode.MEMBER_MISMATCH_AUTHOR)
+fun checkAuthor(userPrincipal: UserPrincipal, member: MemberEntity) {
+    if(!userPrincipal.authorities.contains(GrantedAuthority { "ROLE_ADMIN" })) {
+        if( userPrincipal.memberEmail != member.email )
+            throw AuthorMismatchException(ErrorCode.MEMBER_MISMATCH_AUTHOR)
+    }
 }
+
