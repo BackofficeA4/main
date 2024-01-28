5ebf5e474d7ce98f68059eff4cbc81b747d369ad
Myolang <myohanmyolang@gmail.com>
2024-01-28 23:20:17 +0900

    ADD Admin Service

--- src/main/kotlin/com/teama4/a4document/api/admin/controller/AdminController.kt
+++ src/main/kotlin/com/teama4/a4document/api/admin/controller/AdminController.kt
@@ -1,10 +1,32 @@
 package com.teama4.a4document.api.admin.controller
 
+import com.teama4.a4document.api.admin.service.AdminApiService
+import com.teama4.a4document.infra.security.UserPrincipal
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 @RequestMapping("/admin")
-class AdminController {
+class AdminController(
+	private val adminApiService: AdminApiService
+) {
+
+	@GetMapping("/delete/member/{memberEmail}")
+	fun deleteMember(@AuthenticationPrincipal userPrincipal: UserPrincipal, @PathVariable memberEmail: String) {
+		adminApiService.deleteMember(userPrincipal, memberEmail)
+	}
+
+	@GetMapping("/delete/post/{postId}")
+	fun deletePost(@AuthenticationPrincipal userPrincipal: UserPrincipal, @PathVariable postId: Long) {
+		adminApiService.deletePost(postId, userPrincipal)
+	}
+
+	@GetMapping("/delete/comment/{commentId}")
+	fun deleteComment(@AuthenticationPrincipal userPrincipal: UserPrincipal, @PathVariable commentId:Long) {
+		adminApiService.deleteComment(commentId, userPrincipal)
+	}
 }
--- src/main/kotlin/com/teama4/a4document/common/member/type/UserRole.kt
+++ src/main/kotlin/com/teama4/a4document/common/member/type/UserRole.kt
@@ -1,6 +1,6 @@
 package com.teama4.a4document.common.member.type
 
 enum class UserRole {
-	USER,
-	ADMIN
+	ROLE_USER,
+	ROLE_ADMIN
 }
